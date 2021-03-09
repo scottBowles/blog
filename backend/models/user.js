84a51f86ff653d044bc69059866f6c8266aec278
scottBowles <shbowles@gmail.com>
2021-03-08 22:26:33 -0500

    Add to validators

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema(
   {
     firstName: { type: String, required: true, minlength: 1, maxlength: 255 },
     lastName: { type: String, required: true, minlength: 1, maxlength: 255 },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, maxlength: 255, unique: true },
     password: { type: String, required: true, minlength: 8, maxlength: 1024 },
     isAdmin: Boolean,
   },
@@ -18,7 +18,7 @@ UserSchema.methods.generateAuthToken = function () {
     { _id: this._id, isAdmin: this.isAdmin },
     process.env.JWT_PRIVATE_KEY
   );
-}
+};
 
 UserSchema.virtual('fullName').get(function () {
   return `${this.firstName} ${this.lastName}`;
@@ -30,8 +30,8 @@ export function validateUser(user) {
   const schema = Joi.object({
     firstName: Joi.string().min(1).max(255).required(),
     lastName: Joi.string().min(1).max(255).required(),
-    email: Joi.string().email().max(255),
-    password: Joi.string().min(8).max(255),
+    email: Joi.string().email().max(255).required(),
+    password: Joi.string().min(8).max(255).required(),
     isAdmin: Joi.boolean(),
   });
   return schema.validate(user);
